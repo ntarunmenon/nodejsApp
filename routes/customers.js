0c12b66f4a0ee6cd4e77587f954b078831b8b4e3
ntarunmenon <ntarunmenon@gmail.com>
2018-06-09 21:28:37 +1000

    Customer API completed with tests

--- routes/customers.js
+++ routes/customers.js
@@ -17,9 +17,7 @@ const Customer = mongoose.model('Customer',new mongoose.Schema({
     },
     phone: {
         type: String,
-        required: true,
-        minlength:5,
-        maxlength:5
+        required: true
     }
   }));
 
@@ -27,7 +25,7 @@ const Customer = mongoose.model('Customer',new mongoose.Schema({
     const schema = {
       name: Joi.string().min(3).required(),
       phone: Joi.string().min(7).max(7).required(),
-      phone: Joi.boolean().required()
+      isGold: Joi.boolean().required()
     };
     return Joi.validate(customer, schema);
   }
@@ -38,10 +36,36 @@ const Customer = mongoose.model('Customer',new mongoose.Schema({
   
     let customer = new Customer ({
       name: req.body.name,
-      isGold: req.body.name,
-      name: req.body.name
+      isGold: req.body.isGold,
+      phone: req.body.phone
     });
     customer = await customer.save();
+    res.send(customer);
+  });
+
+  router.get('/', async (req, res) => {
+    const customers = await Customer.find().sort('name');
+    res.send(customers);
+  });
+
+  router.delete('/:id', async (req, res) => {
+    
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+    
+    if (!customer) return res.status(404).send('The Customer with the given ID was not found.');
+
+    res.send(customer);
+  });
+
+  router.put('/:id', async (req, res) => {
+    const { error } = validateCustomer(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const customer = await Customer.findByIdAndUpdate(req.params.id,{name: req.body.name},{
+      new :  true
+    })
+    if (!customer) return res.status(404).send('The Customer with the given ID was not found.');
+
     res.send(customer);
   });
 
